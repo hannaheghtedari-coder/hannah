6bc4eeaeef362d11c2fe4f6aeaf5465e5d8cd753
Hannah Eghtedari <hannaheghtedari@berkeley.edu>
2026-04-06 11:00:15 -0700

    trying to add homework8 folder with test file

---
+++ homework8/testfile.py
@@ -0,0 +1,9 @@
+# this is to ensure that git will actually recognize this branch as valid, hope it works
+
+# adding python commands
+
+def hello(name):
+    print(f"Hello, {name}!")
+
+hello(Hannah)
+
