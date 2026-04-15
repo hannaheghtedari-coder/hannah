6a452ff809dac7b0b9882ad344b227ebd9d2aba8
Hannah Eghtedari <hannaheghtedari@berkeley.edu>
2026-04-14 17:44:56 -0700

    adding hw8_changes.png and getting rid of testfile

--- homework8/testfile.py
+++
@@ -1,9 +0,0 @@
-# this is to ensure that git will actually recognize this branch as valid, hope it works
-
-# adding python commands
-
-def hello(name):
-    print(f"Hello, {name}!")
-
-hello(Hannah)
-
